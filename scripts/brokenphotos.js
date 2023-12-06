1b1b6a0ff1f11d4a570055d94f203fdfe77489c1
Joey Kinnan <jmkappinv@gmail.com>
2023-12-05 22:46:16 -0500

    DEADLANDS DAY

--- scripts/brokenphotos.js
+++
@@ -1,9 +0,0 @@
-const divs = document.querySelectorAll('.galleryPhoto');
-
-divs.forEach(el => el.addEventListener('click', event => {
-    
-    
-    event.target.src = document.getElementById("mainPhoto").src
-    
-
-}));
